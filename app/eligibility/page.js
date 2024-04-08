34cc317f94e9b2450764fc5024db4a19c5e62096
victor <vicopem@gmail.com>
2024-04-08 01:14:50 -0400

    Fixes

--- app/eligibility/page.js
+++ app/eligibility/page.js
@@ -4,23 +4,25 @@ import Header from "@/components/UI/Header";
 import StageIntro from "@/components/sections/eligibility/StageIntro";
 import Stage1 from "@/components/sections/eligibility/Stage1";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 const Elegibility = () => {
   const searchParams = useSearchParams();
+  const [stage, setStage] = useState("");
   const router = useRouter();
-  const stage = searchParams.get("stage");
   useEffect(() => {
-    if (!stage) router.push("/eligibility?stage=start");
+    const stageRoute = searchParams.get("stage");
+    if (!stageRoute) router.push("/eligibility?stage=start");
+    else setStage(stageRoute);
   }, []);
 
   return (
-    <>
+    <Suspense>
       <Header />
       {stage === "start" && <StageIntro />}
       {stage === "1" && <Stage1 />}
       <Footer />
-    </>
+    </Suspense>
   );
 };
 
