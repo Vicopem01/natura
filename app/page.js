af6ff74af6c316c89bacb3e3207b4e526165bbfc
victor <vicopem@gmail.com>
2024-11-09 20:40:46 -0500

    Fix: Naming conflicts

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Footer from "@/components/UI/Footer";
 import Header from "@/components/UI/Header";
 import ChooseUs from "@/components/sections/home/ChooseUs";
 import Customer from "@/components/sections/home/Customer";
-import FAQS from "@/components/sections/home/qss";
+import FAQS from "@/components/sections/home/FAQS";
 import Hero from "@/components/sections/home/Hero";
 import HowItWorks from "@/components/sections/home/HowItWorks";
 import Upgrade from "@/components/sections/home/Upgrade";
